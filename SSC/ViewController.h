371ffb81e580c7a18b15a6c24c6a555f848a9bfe
ShengHao <641776671@qq.com>
2016-09-07 20:15:29 +0800

    Initial Commit

---
+++ SSC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSC
+//
+//  Created by 盛浩 on 16/9/7.
+//  Copyright © 2016年 Hao Sheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
